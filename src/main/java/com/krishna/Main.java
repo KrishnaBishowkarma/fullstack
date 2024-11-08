f4ab610c6091d38177e5f5a42e4c5d7f522a1839
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-08 13:16:38 +0545

    feat: changing GreetResponse class to a record and also changing @ComponentScan, @EnableAutoConfiguration and @Configuration to one single annotation as @SpringBootApplication that includes all the three annotations

--- src/main/java/com/krishna/Main.java
+++ src/main/java/com/krishna/Main.java
@@ -1,18 +1,14 @@
 package com.krishna;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-
-//@SpringBootApplication
-@ComponentScan(basePackages = "com.krishna")
-@EnableAutoConfiguration
-@Configuration
+@SpringBootApplication
+//@ComponentScan(basePackages = "com.krishna")
+//@EnableAutoConfiguration
+//@Configuration
 @RestController
 public class Main {
     public static void main(String[] args) {
@@ -24,38 +20,38 @@ public class Main {
         return new GreetResponse("Hello There");
     }
 
-//    public record GreetResponse(String greet) {
-//    }
-
-    class GreetResponse {
-        public GreetResponse(String greet) {
-            this.greet = greet;
-        }
-
-        private final String greet;
-
-        public String getGreet() {
-            return greet;
-        }
-
-        @Override
-        public String toString() {
-            return "GreetResponse{" +
-                    "greet='" + greet + '\'' +
-                    '}';
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            GreetResponse response = (GreetResponse) o;
-            return Objects.equals(greet, response.greet);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(greet);
-        }
+    public record GreetResponse(String greet) {
     }
+
+//    class GreetResponse {
+//        public GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        private final String greet;
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse response = (GreetResponse) o;
+//            return Objects.equals(greet, response.greet);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hashCode(greet);
+//        }
+//    }
 }
