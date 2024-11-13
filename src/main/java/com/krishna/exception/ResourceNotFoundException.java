f012d2d4513e201b3e40218f7db0be4f74c9b1cf
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-13 10:14:21 +0545

    fix: set HTTP status to 404 for ResourceNotFoundException using ResponseStatus annotation

--- src/main/java/com/krishna/exception/ResourceNotFoundException.java
+++ src/main/java/com/krishna/exception/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package com.krishna.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
