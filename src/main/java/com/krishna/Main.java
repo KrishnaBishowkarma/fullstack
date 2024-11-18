076f512afe9662221955bed26037880b0c54c650
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-18 20:15:14 +0545

    feat: add CommandLineRunner to save initial customer data

--- src/main/java/com/krishna/Main.java
+++ src/main/java/com/krishna/Main.java
@@ -1,7 +1,13 @@
 package com.krishna;
 
+import com.krishna.customer.Customer;
+import com.krishna.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 
@@ -9,5 +15,24 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository) {
+        return args -> {
+            Customer krishna = new Customer(
+                    "Krishna",
+                    "krishna@gmail.com",
+                    25
+            );
+
+            Customer janaki = new Customer(
+                    "Janaki",
+                    "janaki@gmail.com",
+                    28
+            );
+
+            List<Customer> customers = List.of(krishna, janaki);
+            customerRepository.saveAll(customers);
+        };
     }
 }
