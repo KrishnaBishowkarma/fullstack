555727bf83bbae41d87ab20c42db33fdbdee2f07
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-19 13:21:45 +0545

    refactor: centralize @RequestMapping to 'api/v1/customer' and remove redundant usage

--- src/main/java/com/krishna/customer/CustomerController.java
+++ src/main/java/com/krishna/customer/CustomerController.java
@@ -1,17 +1,16 @@
 package com.krishna.customer;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("api/v1/customers")
 public class CustomerController {
 
     private final CustomerService customerService;
 
-    
+
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
@@ -23,12 +22,12 @@ public class CustomerController {
     This is the same as below :
     */
 
-    @GetMapping("api/v1/customers")
+    @GetMapping
     public List<Customer> getCustomers() {
         return customerService.getAllCustomers();
     }
 
-    @GetMapping("api/v1/customers/{customerId}")
+    @GetMapping("{customerId}")
     public Customer getCustomer(
             @PathVariable("customerId") Integer customerId) {
         return customerService.getCustomer(customerId);
