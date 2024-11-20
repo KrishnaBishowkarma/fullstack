296c8d0c47600c9027358f4862125ca628ef879b
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-11-20 19:58:18 +0545

    feat: add methods for checking customer existence and deleting by ID in CustomerDao

--- src/main/java/com/krishna/customer/CustomerDao.java
+++ src/main/java/com/krishna/customer/CustomerDao.java
@@ -5,9 +5,15 @@ import java.util.Optional;
 
 public interface CustomerDao {
     List<Customer> selectAllCustomers();
+
     Optional<Customer> selectCustomerById(Integer id);
 
     void insertCustomer(Customer customer);
 
     boolean existsPersonWithEmail(String email);
+
+    boolean existsPersonWithId(Integer id);
+
+    void deleteCustomerById(Integer customerId);
+
 }
