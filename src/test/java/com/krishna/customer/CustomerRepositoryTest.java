5a9fed7c12fe5c4dd88a2d56575518bdaae867df
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-12-18 23:53:02 +0545

    chore: add test class for CustomerRepository

---
+++ src/test/java/com/krishna/customer/CustomerRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.krishna.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void existsCustomerByEmail() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+
+    @Test
+    void existsCustomerById() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+}
