4600a57c1003e931d96e6140b4e4e630f8a48724
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-12-19 22:57:24 +0545

    test: add the wrong way to test JPA repositories

--- src/test/java/com/krishna/customer/CustomerRepositoryTest.java
+++ src/test/java/com/krishna/customer/CustomerRepositoryTest.java
@@ -2,13 +2,22 @@ package com.krishna.customer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository underTest;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @BeforeEach
     void setUp() {
+        System.out.println(applicationContext.getBeanDefinitionCount());
     }
 
     @Test
