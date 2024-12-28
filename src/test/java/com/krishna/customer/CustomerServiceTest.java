d9d34725e37d57022d071552d325100ff433cd03
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2024-12-28 09:52:57 +0545

    test: add unit test for getCustomer method in CustomerService class

--- src/test/java/com/krishna/customer/CustomerServiceTest.java
+++ src/test/java/com/krishna/customer/CustomerServiceTest.java
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,12 +35,22 @@ class CustomerServiceTest {
     }
 
     @Test
-    void getCustomer() {
+    void canGetCustomer() {
         // GIVEN
+        int id = 10;
+        Customer customer = new Customer(
+                12,
+                "krishna",
+                "dai@gmail.com",
+                12
+        );
+        when(customerDao.selectCustomerById(id)).thenReturn(Optional.of(customer));
 
         // WHEN
+        Customer actual = underTest.getCustomer(10);
 
         // THEN
+        assertThat(actual).isEqualTo(customer);
     }
 
     @Test
