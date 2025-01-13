ef81dbdd45ed86dc03af9327748ab8315871c085
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-01-13 09:40:00 +0545

    test: add WebTestClient for integration testing
                   - Added WebTestClient to facilitate testing of reactive endpoints.
                   - Configured with @Autowired for integration tests.

--- src/test/java/com/krishna/journey/CustomerIntegrationTest.java
+++ src/test/java/com/krishna/journey/CustomerIntegrationTest.java
@@ -1,10 +1,14 @@
 package com.krishna.journey;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class CustomerIntegrationTest {
 
+    @Autowired
+    private WebTestClient webTestClient;
 }
