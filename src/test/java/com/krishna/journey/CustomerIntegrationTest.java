1763647475f7a7a779585bda607d987b821e3ff2
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-01-13 10:11:34 +0545

    test: add test for customer registration using Faker and Random for random data
                   - Implemented canRegisterACustomer test method to simulate customer registration.
                   - Used Faker library to generate random names, emails and Random for the registration request.
                   - Ensured the registration request is correctly constructed with dynamic test data.

--- src/test/java/com/krishna/journey/CustomerIntegrationTest.java
+++ src/test/java/com/krishna/journey/CustomerIntegrationTest.java
@@ -1,14 +1,46 @@
 package com.krishna.journey;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
+import com.krishna.customer.CustomerRegistrationRequest;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Random;
+import java.util.UUID;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class CustomerIntegrationTest {
 
+    private final Random RANDOM = new Random();
+
     @Autowired
     private WebTestClient webTestClient;
+
+    @Test
+    void canRegisterACustomer() {
+        // create a registration request
+        Faker faker = new Faker();
+        Name fakerName = faker.name();
+        String name = fakerName.fullName();
+        String email = fakerName.lastName() + UUID.randomUUID() + "@krishna-bk.com.np";
+        int age = RANDOM.nextInt(1, 100);
+
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(
+                name,
+                email,
+                age
+        );
+
+        // send a post request to /customers
+        // get all customers
+        // make sure customer is present
+        // get customer by id
+    }
 }
