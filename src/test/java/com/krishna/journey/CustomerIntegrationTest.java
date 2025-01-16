aff12203b83e072f8c54e2bb11396fc99b0d7199
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-01-16 22:36:13 +0545

    test: add integration test for deleting a customer by ID

--- src/test/java/com/krishna/journey/CustomerIntegrationTest.java
+++ src/test/java/com/krishna/journey/CustomerIntegrationTest.java
@@ -61,7 +61,8 @@ public class CustomerIntegrationTest {
                 .exchange()
                 .expectStatus()
                 .isOk()
-                .expectBodyList(new ParameterizedTypeReference<Customer>() {})
+                .expectBodyList(new ParameterizedTypeReference<Customer>() {
+                })
                 .returnResult()
                 .getResponseBody();
 
@@ -90,12 +91,69 @@ public class CustomerIntegrationTest {
                 .exchange()
                 .expectStatus()
                 .isOk()
-                .expectBody(new ParameterizedTypeReference<Customer>() {})
+                .expectBody(new ParameterizedTypeReference<Customer>() {
+                })
                 .isEqualTo(expectedCustomer);
     }
 
     @Test
     void canDeleteCustomer() {
+        // create a registration request
+        Faker faker = new Faker();
+        Name fakerName = faker.name();
 
+        String name = fakerName.fullName();
+        String email = fakerName.lastName() + " - " + UUID.randomUUID() + "@krishna-bk.com.np";
+        int age = RANDOM.nextInt(1, 100);
+
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(
+                name,
+                email,
+                age
+        );
+
+        // send a post request to /customers
+        webTestClient.post()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        // get all customers
+        List<Customer> allCustomers = webTestClient.get()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(new ParameterizedTypeReference<Customer>() {
+                })
+                .returnResult()
+                .getResponseBody();
+
+
+        int id = allCustomers.stream()
+                .filter(customer -> customer.getEmail().equals(email))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        // delete customer
+        webTestClient.delete().uri(CUSTOMER_URI + "/{id}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        // get customer by id
+        webTestClient.get()
+                .uri(CUSTOMER_URI + "/{id}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isNotFound();
     }
 }
